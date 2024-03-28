2a37fa85085b134fcb8b2e0e1f08e0200b868349
audiofolks <audiofolks@gmail.com>
2024-03-28 22:15:29 +0530

    add a middleware and a funtion to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,26 +1,33 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
-    const  { username, email, password } = req.body;
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-    if (!username || !email || !password || username === ' ' || email === ' ' || password === ' '){
-        return res.status(400).json({ message: 'All fields are required' });
-    }
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === " " ||
+    email === " " ||
+    password === " "
+  ) {
+    next(errorHandler(400, 'All fields are requred' ))
+  }
 
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword,
-    });
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-try {
+  try {
     await newUser.save();
-    res.json({message: 'Signup successull'});
-} catch (error){
-    res.status(500).json({message: error.message})
-}
-
+    res.json({ message: "Signup successull" });
+  } catch (error) {
+    next(error);
+  }
 };
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,24 +1,25 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        requiured: true,
-        unique: true,
+      type: String,
+      requiured: true,
+      unique: true,
     },
     email: {
-        type: String,
-        requiured: true,
-        unique: true,
+      type: String,
+      requiured: true,
+      unique: true,
     },
     password: {
-        type: String,
-        requiured: true,
+      type: String,
+      requiured: true,
     },
-}, {timestamps: true}
-
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
