fb55f4154688951a29ba7bcd93f32ed5d7383a93
audiofolks <audiofolks@gmail.com>
2024-03-29 12:18:56 +0530

    rectify errors

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,17 @@
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
-import { BsInstagram, BsDiscord, BsYoutube, BsLinkedin, BsGithub, } from "react-icons/bs";
+import {
+  BsInstagram,
+  BsDiscord,
+  BsYoutube,
+  BsLinkedin,
+  BsGithub,
+} from "react-icons/bs";
 
 export default function FooterCom() {
   return (
     <Footer container className="border border-t-8 border-teal-500">
-      <div className="w-full max-w-7xl mx-audo">
+      <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
           <div className="mt-5">
             <Link
